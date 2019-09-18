1949763516c4089d460077a41c3b9ee987070800
Quan Li <quan.li@williamoneilchina.com>
2019-09-18 17:54:55 +0800

    添加AES 加密

--- AppUtils/Extension/StringExtension.swift
+++ AppUtils/Extension/StringExtension.swift
@@ -13,21 +13,6 @@ public extension String{
         return NSRange.init(location: 0, length: self.count)
     }
     
-    func md5() -> String {
-        let str = self.cString(using: String.Encoding.utf8)
-        let strLen = CUnsignedInt(self.lengthOfBytes(using: String.Encoding.utf8))
-        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-        let result = UnsafeMutablePointer<UInt8>.allocate(capacity: 16)
-        CC_MD5(str!, strLen, result)
-        let hash = NSMutableString()
-        for i in 0 ..< digestLen {
-            hash.appendFormat("%02x", result[i])
-        }
-        free(result)
-        return String(format: hash as String)
-    }
-    
-    
     func isMatched(pattern:String) -> Bool{
         if NSPredicate(format: "SELF MATCHES %@", pattern).evaluate(with: self) {
             return true
@@ -61,6 +46,20 @@ public extension String{
         return formatter.string(from: number)
     }
     
+    
+    func md5() -> String {
+        let str = self.cString(using: String.Encoding.utf8)
+        let strLen = CUnsignedInt(self.lengthOfBytes(using: String.Encoding.utf8))
+        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<UInt8>.allocate(capacity: 16)
+        CC_MD5(str!, strLen, result)
+        let hash = NSMutableString()
+        for i in 0 ..< digestLen {
+            hash.appendFormat("%02x", result[i])
+        }
+        free(result)
+        return String(format: hash as String)
+    }
 }
 
 
--- LQAppUtils.podspec
+++ LQAppUtils.podspec
@@ -1,16 +1,16 @@
 Pod::Spec.new do |spec|
   spec.name         = 'LQAppUtils'
 
-  spec.version      = '1.1.13'
+  spec.version      = '1.1.14'
   spec.license      = 'MIT'
   spec.author       = { "Quan Li" => "1083099465@qq.com" }
   spec.summary      = 'iOS 公共工具'
   spec.homepage     = 'https://github.com/lqIphone/AppUtils'
-  spec.source       = { :git => 'https://github.com/lqIphone/AppUtils.git', :tag => '1.1.13' }
+  spec.source       = { :git => 'https://github.com/lqIphone/AppUtils.git', :tag => '1.1.14' }
   spec.source_files  = "AppUtils/**/*.swift"
   spec.requires_arc = true
   spec.platform     = :ios, "9.0"
-  spec.swift_version = '5.0'
+  spec.swift_version = '4.2'
   spec.framework  = "Security"
   spec.framework  = "WebKit"
   spec.dependency "SwifterSwift"
