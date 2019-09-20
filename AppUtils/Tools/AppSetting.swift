11b284ec73df6e93ea29f9f65473b8b7e0844b9f
Quan Li <quan.li@williamoneilchina.com>
2019-09-20 13:53:43 +0800

    添加网络切换VC

--- AppUtils/Tools/AppSetting.swift
+++ AppUtils/Tools/AppSetting.swift
@@ -7,13 +7,69 @@
 //
 
 import Foundation
- public struct AppSetting {
-    public static func getString(key:String) -> String{
-        return UserDefaults.standard.value(forKey: key)  as? String ?? ""
+public struct AppSetting {
+    public static func getString(key:String) -> String?{
+        return UserDefaults.standard.string(forKey: key)
     }
     
     public static func setString(key:String,value:String){
         return UserDefaults.standard.set(value, forKey: key)
+    }
+    
+    public static func getArray(key:String) -> [Any]?{
+        return UserDefaults.standard.array(forKey: key)
+    }
+    
+    public static func setArray(key:String,value:[Any]){
+        return UserDefaults.standard.set(value, forKey: key)
+    }
+    
+    public static func getDictionary(key:String) -> [String : Any]?{
+        return UserDefaults.standard.dictionary(forKey: key)
+    }
+    
+    public static func setDictionary(key:String,value:[String : Any]){
+        return UserDefaults.standard.set(value, forKey: key)
+    }
+    
+    public static func getData(key:String) -> Data?{
+        return UserDefaults.standard.data(forKey: key)
+    }
+    
+    public static func setData(key:String,value:Data){
+        return UserDefaults.standard.set(value, forKey: key)
+    }
+    
+    public static func getStringArray(key:String) -> [String]?{
+        return UserDefaults.standard.stringArray(forKey: key)
+    }
+    
+    public static func setStringArray(key:String,value:[String]){
+        return UserDefaults.standard.set(value, forKey: key)
+    }
+    
+    public static func getInt(key:String) -> Int {
+        return UserDefaults.standard.integer(forKey: key)
+    }
+    
+    public static func setInt(key:String,value:Int){
+        UserDefaults.standard.set(value, forKey: key)
+    }
+    
+    public static func getFloat(key:String) -> Float {
+        return UserDefaults.standard.float(forKey: key)
+    }
+    
+    public static func setFloat(key:String,value:Float){
+        UserDefaults.standard.set(value, forKey: key)
+    }
+    
+    public static func getDouble(key:String) -> Double {
+        return UserDefaults.standard.double(forKey: key)
+    }
+    
+    public static func setDouble(key:String,value:Double){
+        UserDefaults.standard.set(value, forKey: key)
     }
     
     public static func getBool(key:String) -> Bool {
@@ -24,11 +80,11 @@ import Foundation
         UserDefaults.standard.set(value, forKey: key)
     }
     
-    public static func getInt(key:String) -> Int {
-        return UserDefaults.standard.integer(forKey: key)
+    public static func getURL(key:String) -> URL? {
+        return UserDefaults.standard.url(forKey: key)
     }
     
-    public static func setInt(key:String,value:Int){
+    public static func setURL(key:String,value:URL){
         UserDefaults.standard.set(value, forKey: key)
     }
 }
